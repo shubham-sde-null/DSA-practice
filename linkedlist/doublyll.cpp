b9d904f45f3149810b179392de1ed22aaab428aa
shubham-sde-null <shubhamsdecpporigin@gmail.com>
2022-08-04 19:31:33 +0530

    circular linked list

--- linkedlist/doublyll.cpp
+++ linkedlist/doublyll.cpp
@@ -12,6 +12,16 @@ Node(int d){
     this->previous=NULL;
     this->next=NULL;
 }
+
+//destructor for freeying memory
+~Node(){
+    int value=this->data;
+    if(this->next!=NULL){
+        delete next;
+        this->next=NULL;
+    }
+    cout<<"memory is free at the node with value:"<<value<<endl;
+}
 };
 void printNodes(Node* &head){
     Node* temp=head;
@@ -32,8 +42,14 @@ int getLengthOfLl(Node* &head){
     }
     return len;
 }
-void insertAtHead(Node* &head, int d){
-    // Node*temp=head;
+void insertAtHead(Node* &head,Node* &tail, int d){
+    //Here I am writing this case because in case my linked list is empty it means there is head and tail are empty so we will create a node which will be considered as head.
+    if(head==NULL){
+        Node* temp=new Node(d);
+        head=temp;
+        tail=temp;
+    }
+    else{
     Node* temp=new Node(d);
     temp->previous=NULL;
     temp->next=head;
@@ -41,21 +57,33 @@ void insertAtHead(Node* &head, int d){
     // head->next=NULL;
     //this step is not necessary, because earlier also head's next was temp
     head=temp;
+    }
+
 
 }
-void insertAtTail(Node* &tail, int d){
+void insertAtTail(Node* &head,Node* &tail, int d){
+    //here also we are doing the same thing when our head and tail is empty which means our linked list is empty
+    if(tail==NULL){
+        Node* temp=new Node(d);
+        tail=temp;
+        head=temp;
+    }
+    else{
     Node* temp=new Node(d);
     tail->next=temp;
     temp->previous=tail;
     tail=temp;
     // temp=tail; If I write like this i.e temp=tail then I won't get my result
     temp->next=NULL;
+
+    }
+
 }
 void inserAtPosition(Node* head,Node*tail, int position, int d){
 
     //inserting at firsy node
     if(position==1){
-        insertAtHead(head,d);
+        insertAtHead(head,tail,d);
         return;
     }
    Node* temp =head;
@@ -66,7 +94,7 @@ void inserAtPosition(Node* head,Node*tail, int position, int d){
    }
    //inserting at last node
    if(temp->next==NULL){
-    insertAtTail(tail,d);
+    insertAtTail(head,tail,d);
     return;
    }
    Node* nodeToInsert=new Node(d);
@@ -77,18 +105,56 @@ void inserAtPosition(Node* head,Node*tail, int position, int d){
    nodeToInsert->previous=temp;
 
 }
+
+//deleting the node from head
+void deleteAtPosition(int position,Node* &head){
+    //here we are deleting the first node
+    if(position==1){
+    Node*temp=head;
+    temp->next->previous=NULL;
+    head=temp->next;
+    temp->next=NULL;
+    //here we are freeying the memory
+    delete temp;
+    }
+    //here we are deleting any node except the last node
+    else{
+        //here we are making two node one is head and the other node is previous of current node which means it is null
+        Node* current=head;
+        Node* previous=NULL;
+        int count=1;
+        while(count<position){
+            previous=current;
+            current=current->next;
+            //here since we have to move both previous and current forward so we will make previous equal to current and current to current->next
+            count++;
+        }
+        current->previous=NULL;
+        previous->next=current->next;
+        current->next=NULL;
+        delete current;
+    }
+
+
+}
 int main(){
     Node* node1=new Node(10);
     Node* head=node1;
     Node* tail=node1;
     printNodes(head);
     // cout<<getLengthOfLl(head);
-    insertAtHead(head,25);
+    insertAtHead(head,tail,25);
     // cout<<endl;
     printNodes(head);
-    insertAtTail(tail,355);
+    insertAtTail(head,tail,355);
     printNodes(head);
     inserAtPosition(head,tail,3,50);
+    printNodes(head);
+    deleteAtPosition(1,head);
+    printNodes(head);
+    deleteAtPosition(2,head);
+    printNodes(head);
+     deleteAtPosition(2,head);
     printNodes(head);
 
 
