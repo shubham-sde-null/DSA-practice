f3977f3d8c5dc46d637db576673c26998d6515ce
shubham-sde-null <shubhamsdecpporigin@gmail.com>
2022-09-26 23:42:44 +0530

    dsa problems

---
+++ binarySearch/splitArrayLargestSum.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+bool isPossible(int arr[],int mid,int n,int m){
+    int sub=1;
+    int sum=0;
+    for(int i=0;i<n;i++){
+      sum+=arr[i];
+      if(sum>mid){
+        sub++;
+        sum=arr[i];
+      }
+    }
+    return sub<=m;
+}
+int main(){
+    int min=INT_MAX;
+    int max=INT_MIN;
+    int arr[5]={7,2,5,10,8};
+    int split=2;
+    int sumOfArray=0;
+    vector<int> lowest;
+    //using the for loop to calculate the min and the max value
+    for(int i=0;i<5;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    for(int i=0;i<5;i++){
+        sumOfArray+=arr[i];
+    }
+    // cout<<"the minimum value in array is:"<<max<<endl;
+    // cout<<"the sum is:"<<sumOfArray<<endl;
+    int s=max;
+    int e=sumOfArray;
+    int mid=s+(e-s)/2;
+    int subArray=0;
+    int ans=0;
+    //here I am considering the case when the number of slpits are equals to the number of elements in the array
+    if(split==5){
+        return max;
+    }
+    while(s<=e){
+        if(isPossible(arr,mid,5,2)==true){
+            //here I can directly show the result i.e which one is lowest among the result
+            ans=mid;
+            // lowest.push_back(mid);
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+
+    }
+    cout<<"the answer is:"<<ans<<endl;
+    //here I am showing all the possible solutions which are coming
+    // for(int i=0;i<lowest.size();i++){
+    //     cout<<lowest[i]<<endl;
+    // }
+return 0;
+}
--- binarySearch/squaresofNumber.cpp
+++ binarySearch/squaresofNumber.cpp
@@ -1,25 +1,92 @@
+// #include<iostream>
+// using namespace std;
+// #include<math.h>
+// bool result(int n){
+//     int s=0;
+//     int e=sqrt(n);
+//     while(s<=e){
+//         if(s*s+e*e==n){
+//             return true;
+//         }
+//         else if(s*s+e*e<n){
+//             s++;
+//         }
+//         else{
+//             e--;
+//         }
+//     }
+//     return false;
+// }
+// int main(){
+//     int n=29;
+//     cout<<result(n)<<endl;
+// return 0;
+// }
 
+//if in question they have given don't use the math function to solve the question
 #include<iostream>
 using namespace std;
-#include<math.h>
-bool result(int n){
+int finalResult(int n){
     int s=0;
-    int e=sqrt(n);
+    int e=n-1;
+    int m=s+(e-s)/2;
+    int ans;
     while(s<=e){
+        if(m*m>n){
+            e=m-1;
+        }
+        else if(m*m<=n){
+            ans=m;
+            s=m+1;
+        }
+        m=s+(e-s)/2;
+    }
+    return ans;
+}
+
+//this way is working but here the time complexity will be O(temp^2)
+// bool solution(int temp,int n){
+//     for(int i=0;i<=temp;i++){
+//     for(int j=temp;j>=0;j--){
+//         if(i*i+j*j==n){
+//             return true;
+//         }
+//     }
+// }
+// return false;
+// }
+
+
+//
+bool solution(int temp,int n){
+    int counter=0;
+    if(n==1){
+        return true;
+    }
+    int s=0;
+    int e=temp;
+    while(s<=temp){
         if(s*s+e*e==n){
+            counter++;
+            cout<<"counter runned for:"<<counter<<" times"<<endl;
             return true;
         }
         else if(s*s+e*e<n){
+            counter++;
             s++;
         }
         else{
+            counter++;
             e--;
         }
     }
+
     return false;
 }
+
 int main(){
-    int n=29;
-    cout<<result(n)<<endl;
+    int n=39;
+    int temp=finalResult(n);
+    cout<<solution(temp,n);
 return 0;
 }
